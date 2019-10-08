ecdf7197d7db218bf8c0f6fbc20738ed8295ef79
fufulong <fufulong@hqjy.com>
2019-10-08 18:15:59 +0800

    Initial commit

---
+++ src/main/java/com/fufulong/DesignPatternDemoApplication.java
@@ -0,0 +1,13 @@
+package com.fufulong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignPatternDemoApplication.class, args);
+    }
+
+}
